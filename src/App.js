1410b04fcaffd79d63393674498acb6ecaaf4cf4
anthonywidjaja8 <30653274+anthonywidjaja8@users.noreply.github.com>
2020-10-08 20:56:08 +0700

    Tugas-9

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Form Pembelian Buah</h1>
+      <table id="form" cellspacing="20px">
+        <tr>
+          <td><b>Nama Pelanggan</b></td>
+          <td><input></input></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><input type="checkbox"/>Semangka</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><input type="checkbox"/>Jeruk</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><input type="checkbox"/>Nanas</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><input type="checkbox"/>Salak</td>
+        </tr>
+        <tr>
+          <td><b>Daftar Item</b></td>
+          <td><input type="checkbox"/>Anggur</td>
+        </tr>
+        <input type="submit" value="Kirim" id="btnKirim"/>
+      </table>
+    </div>
+  );
+}
+
+export default App;
