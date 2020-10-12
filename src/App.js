5617899102bd95a78639dd03668d68b9260b3580
anthonywidjaja8 <30653274+anthonywidjaja8@users.noreply.github.com>
2020-10-12 20:40:14 +0700

    Tugas-11

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-import TableFruitPrice from './Tugas-10/tugas10';
 import Form from './Tugas-9/tugas9';
+import TableFruitPrice from './Tugas-10/tugas10';
+import Timer from './Tugas-11/tugas11'
 
 function App() {
   return (
     <>
       <Form />
       <TableFruitPrice />
+      <Timer />
     </>
   )
 }
